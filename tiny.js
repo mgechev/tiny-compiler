628f1397d2fb809f0aa80f2a3a9841550fe31e9b
mgechev <mgechev@gmail.com>
2017-09-08 13:02:57 +0200

    Fix issue and simplify

--- tiny.js
+++ tiny.js
@@ -1,6 +1,6 @@
 /*
   # Lexer
-
+  
   The lexer is responsible for turning the input string into
   a list of tokens. Usually a token looks the following way:
 
@@ -12,11 +12,9 @@
   ```
 
   In our case we're keeping everything simplified and store
-  only the token's value. Later, we can infer the type based on
+  only the token's value. We can infer the type based on
   regular expressions defined below.
-
 */
-
 const lex = str => str.split(' ').map(s => s.trim()).filter(s => s.length);
 
 /*
@@ -28,13 +26,13 @@ const lex = str => str.split(' ').map(s => s.trim()).filter(s => s.length);
   we use recursive descent parsing to produce the AST
   from the input token array.
 
-  Visually, parsing is a process which will turn the token array:
+  Visually, the parsing is a process which turns the array:
 
   ```javascript
   const tokens = ["-", "2", "+", "1", "3", "4"];
   ```
 
-  into the following tree:
+  to the following tree:
 
   ```
     -
@@ -43,35 +41,17 @@ const lex = str => str.split(' ').map(s => s.trim()).filter(s => s.length);
     1 3 4
   ```
 
-  This parser uses the following grammar to parse the input token array:
+  The parser uses the following grammar to parse the input token array:
 
   ```
   num := 0-9
   op := + | - | / | *
   expr := num | op expr* | (expr)
   ```
-
-  For details see https://en.wikipedia.org/wiki/Backus%E2%80%93Naur_form.
-
 */
 
 const Op = Symbol('op');
 const Num = Symbol('num');
-
-class NumNode {
-  constructor(val) {
-    this.type = Num;
-    this.val = val;
-  }
-}
-
-class OpNode {
-  constructor(val, expr) {
-    this.val = val;
-    this.type = Op;
-    this.expr = expr;
-  }
-}
 
 const opre = /(\+|-|\*|\/)/;
 const numre = /[0-9]/;
@@ -85,14 +65,13 @@ const parse = tokens => {
   const prev = () => tokens[--c];
 
   const parseNum = () => {
-    const node = new NumNode(parseInt(cur()))
+    const node = { val: parseInt(cur()), type: Num };
     next();
     return node;
   };
 
   const parseOp = () => {
-    const op = cur();
-    const node = new OpNode(op, []);
+    const node = { val: cur(), type: Op, expr: [] };
     next();
     while (cur()) node.expr.push(parseExpr());
     return node;
@@ -115,20 +94,19 @@ const parse = tokens => {
 
   # Evaluator
 
-  The evaluator visits each node from the tree with pre-order
-  traversal and either:
+  Finally, this is our evaluator. In it we simply visit each node
+  from the tree with pre-order traversal and either:
 
   - Return the corresponding value, in case the node is of type number.
-  - Perform the corresponding arithmetic operation, in case of a `OpNode`.
+  - Perform the corresponding arithmetic operation, in case of an operation node.
 
 */
-
 const eval = ast => {
-  const reduce = (op, args) => args.reduce((p, c) => op(p, c), 0);
+  const reduce = (op, args, init = 0) => args.reduce(op, init);
   const sum = args => reduce((a, b) => b + a, args);
   const sub = args => reduce((a, b) => b - a, args);
-  const div = args => reduce((a, b) => b / a, args);
-  const mul = args => reduce((a, b) => b * a, args);
+  const div = args => reduce((a, b) => b / a, args, 1);
+  const mul = args => reduce((a, b) => b * a, args, 1);
 
   const opAcMap = {
     '+': sum,
@@ -146,12 +124,11 @@ const eval = ast => {
 /*
   # Interpreter
 
-  In order to interpret the input string we feed the parser with the output
+  In order to interpret the input stream we feed the parser with the input
   from the lexer and the evaluator with the output of the parser.
 
 */
 
-const program = '- 2 + 1 3 4';
+const program = '* 3 - 2 + 1 3 4';
 const ast = parse(lex(program));
 console.log(eval(ast));
-
